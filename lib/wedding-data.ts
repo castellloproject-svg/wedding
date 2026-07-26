e52fd222e62a00b7ec869c2fbf2f5e0a82dac8be
castellloproject-svg <castellloproject@gmail.com>
2026-07-26 17:41:16 +0700

    Create wedding-data.ts

---
+++ lib/wedding-data.ts
@@ -0,0 +1,99 @@
+export const weddingData = {
+  couple: {
+    groom: {
+      nickname: "YUSTIFAN",
+      fullName: "Yustifan",
+      parents: "Putra dari Bapak Nama Ayah & Ibu Nama Ibu",
+      photo: "/images/groom.jpg",
+    },
+
+    bride: {
+      nickname: "LETA",
+      fullName: "Leta",
+      parents: "Putri dari Bapak Nama Ayah & Ibu Nama Ibu",
+      photo: "/images/bride.jpg",
+    },
+  },
+
+  wedding: {
+    title: "THE WEDDING OF",
+    displayDate: "18 • 08 • 2026",
+    date: "2026-08-18T08:00:00+07:00",
+  },
+
+  quote: {
+    text: "Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan untukmu pasangan-pasangan dari jenismu sendiri, supaya kamu merasa tenteram kepadanya, dan dijadikan-Nya di antaramu rasa kasih dan sayang.",
+    source: "QS. Ar-Rum: 21",
+  },
+
+  events: {
+    akad: {
+      title: "Akad Nikah",
+      day: "Selasa",
+      date: "18 Agustus 2026",
+      time: "08.00 WIB",
+      location: "Nama Gedung / Rumah",
+      address: "Alamat lengkap lokasi akad nikah",
+      mapsUrl: "https://www.google.com/maps/search/?api=1&query=Jakarta",
+    },
+
+    reception: {
+      title: "Resepsi",
+      day: "Selasa",
+      date: "18 Agustus 2026",
+      time: "11.00 - 14.00 WIB",
+      location: "Nama Gedung / Rumah",
+      address: "Alamat lengkap lokasi resepsi",
+      mapsUrl: "https://www.google.com/maps/search/?api=1&query=Jakarta",
+    },
+  },
+
+  gallery: [
+    "/images/gallery-01.jpg",
+    "/images/gallery-02.jpg",
+    "/images/gallery-03.jpg",
+    "/images/gallery-04.jpg",
+    "/images/gallery-05.jpg",
+    "/images/gallery-06.jpg",
+  ],
+
+  loveStory: [
+    {
+      year: "01",
+      title: "Perkenalan",
+      description:
+        "Berawal dari sebuah pertemuan sederhana yang kemudian membawa kami pada perjalanan yang penuh cerita.",
+    },
+    {
+      year: "02",
+      title: "Lamaran",
+      description:
+        "Dengan penuh keyakinan dan doa, kami memutuskan untuk melangkah bersama menuju kehidupan baru.",
+    },
+    {
+      year: "03",
+      title: "Menikah",
+      description:
+        "Dengan memohon ridho Allah SWT, kami mengikat janji untuk tumbuh, mencintai, dan berjalan bersama selamanya.",
+    },
+  ],
+
+  bankAccounts: [
+    {
+      bank: "BCA",
+      number: "1234567890",
+      owner: "YUSTIFAN",
+      logo: "/images/bca-logo.png",
+    },
+    {
+      bank: "BNI",
+      number: "0987654321",
+      owner: "LETA",
+      logo: "/images/bni-logo.png",
+    },
+  ],
+
+  music: {
+    src: "/music/wedding-song.mp3",
+  },
+};
