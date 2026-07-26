484168c0ccc7909652a38465039842d62bd2a9b3
castellloproject-svg <castellloproject@gmail.com>
2026-07-26 17:51:06 +0700

    Create FloralDecoration.tsx

---
+++ components/FloralDecoration.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+export default function FloralDecoration() {
+  return (
+    <>
+      <img
+        src="/decorations/floral-top-left.png"
+        alt=""
+        className="floral-decoration floral-top-left"
+      />
+
+      <img
+        src="/decorations/floral-top-right.png"
+        alt=""
+        className="floral-decoration floral-top-right"
+      />
+
+      <img
+        src="/decorations/floral-bottom-left.png"
+        alt=""
+        className="floral-decoration floral-bottom-left"
+      />
+
+      <img
+        src="/decorations/floral-bottom-right.png"
+        alt=""
+        className="floral-decoration floral-bottom-right"
+      />
+    </>
+  );
+}
